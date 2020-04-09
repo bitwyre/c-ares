890d254063bcfa3e855c12b1739b453662a00e29
Agustin K-ballo Berge <k@fusionfenix.com>
2020-04-09 10:44:53 -0300

    Conan recipe c-ares/1.16.0-beta15@bitwyre/stable

---
+++ conanfile.py
@@ -0,0 +1,45 @@
+from conans import ConanFile, CMake
+
+
+class CAresConan(ConanFile):
+    name = "c-ares"
+    version = "1.16.0"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of CAres here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "use_pic": [True, False]
+    }
+    default_options = {"shared": False, "use_pic": False}
+    generators = "cmake"
+    exports_sources = "*"
+    no_copy_source = True
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions['BUILD_SHARED_LIBS'] = self.options.shared
+        cmake.definitions['BUILD_TESTING'] = False
+        cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.use_pic
+        cmake.definitions['CARES_BUILD_CONTAINER_TESTS'] = False
+        cmake.definitions['CARES_BUILD_TESTS'] = False
+        cmake.definitions['CARES_BUILD_TOOLS'] = False
+        cmake.definitions['CARES_SHARED'] = False
+        cmake.definitions['CARES_STATIC'] = True
+        cmake.definitions['CARES_STATIC_PIC'] = self.options.use_pic
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["cares"]
